ee6a85af6c380f282ec68b41e8199f341257a716
Christopher Trojan <trojancj@uwec.edu>
2017-07-09 13:32:47 -0500

    Initial Commit

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Rigidbody rb;
+    public float forwardsForce = 2000f;
+    public float sidewaysForce = 500f;
+	
+	void FixedUpdate () {
+	    rb.AddForce(0, 0, forwardsForce * Time.deltaTime);	
+
+        if(Input.GetKey("d")){
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+        if(Input.GetKey("a")){
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public Movement movement;
+
+    void OnCollisionEnter (Collision collisionInfo){
+        if(collisionInfo.collider.tag == "New tag"){
+           movement.enabled = false;
+           Debug.Log("We hit something.");
+        }
+    }
+
+}
